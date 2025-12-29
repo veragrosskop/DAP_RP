dadbbb59b01581b31604059a0eea40ee3868b1c0
emild <emildohne@gmail.com>
2025-12-29 21:16:29 +0100

    [DP-14] Refactor UI

--- main.py
+++ main.py
@@ -2,20 +2,15 @@ import sys
 
 from PySide6 import QtWidgets
 
-from dap_player.ui.main_menu import DAPScreenUI, Theme
+from dap_player.ui.main_menu import DAPScreenUI
+from dap_player.ui.style.theme import Stylesheet, Theme
 
 # ----------------------
 # Run app
 # ----------------------
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    # Load and apply QSS stylesheet
-    try:
-        app.setStyleSheet(Theme.setTheme("purple"))
-        # with open("dap_player/style.qss", "r") as f:
-        #     app.setStyleSheet(f.read())
-    except Exception as e:
-        print(f"Failed to load QSS: {e}")
     window = DAPScreenUI()
+    window.setStyleSheet(Stylesheet.stylesheet(Theme.green))
     window.show()
     sys.exit(app.exec())
